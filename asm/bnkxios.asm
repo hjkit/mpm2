2c76f48d4b3179347e1e962889dae92aa1a2be93
Aaron Wohl <wohl@awohl.com>
2025-12-28 08:29:55 -0500

    remove scraps that confuse claude

--- asm/bnkxios.asm
+++ asm/bnkxios.asm
@@ -1,305 +1,448 @@
-	title	'MP/M II V2.0  Emulator XIOS'
-	cseg
+; bnkxios_port.asm - MP/M II BNKXIOS using I/O port dispatch
+; Part of MP/M II Emulator
+; SPDX-License-Identifier: GPL-3.0-or-later
 ;
-; BNKXIOS for MP/M II Emulator
-; This XIOS is designed to be intercepted by the emulator.
-; Most functions just return - the emulator handles the real work
-; by trapping on the PC address.
+; Complete Banked XIOS for MP/M II using I/O port traps.
+; Each entry point does: LD A, function; OUT (0xE0), A; RET
+; The emulator reads A register to determine which function to call.
 ;
-; Assemble with RMAC, link with LINK to create BNKXIOS.SPR
+; This is designed to be loaded as BNKXIOS.SPR or patched into memory.
 ;
-false	equ	0
-true	equ	not false
+; Assemble with: um80 -o bnkxios_port.rel bnkxios_port.asm
+;                ul80 --prl -o BNKXIOS_port.SPR bnkxios_port.rel
 
-;
-; Number of consoles supported
-;
-nmbcns	equ	8		; 8 consoles for SSH users
+        .Z80                    ; Use Z80 mnemonics
+        ; No ORG - this is a relocatable SPR/PRL file
 
-;
+; I/O Ports
+XIOS_DISPATCH:  EQU     0E0H    ; XIOS dispatch (A = function)
+BANK_SELECT:    EQU     0E1H    ; Bank select (A = bank)
+
+; XIOS function offsets (must match xios.h constants)
+FUNC_BOOT:      EQU     00H
+FUNC_WBOOT:     EQU     03H
+FUNC_CONST:     EQU     06H
+FUNC_CONIN:     EQU     09H
+FUNC_CONOUT:    EQU     0CH
+FUNC_LIST:      EQU     0FH
+FUNC_PUNCH:     EQU     12H
+FUNC_READER:    EQU     15H
+FUNC_HOME:      EQU     18H
+FUNC_SELDSK:    EQU     1BH
+FUNC_SETTRK:    EQU     1EH
+FUNC_SETSEC:    EQU     21H
+FUNC_SETDMA:    EQU     24H
+FUNC_READ:      EQU     27H
+FUNC_WRITE:     EQU     2AH
+FUNC_LISTST:    EQU     2DH
+FUNC_SECTRAN:   EQU     30H
+FUNC_SELMEM:    EQU     33H
+FUNC_POLLDEV:   EQU     36H
+FUNC_STARTCLK:  EQU     39H
+FUNC_STOPCLK:   EQU     3CH
+FUNC_EXITRGN:   EQU     3FH
+FUNC_MAXCON:    EQU     42H
+FUNC_SYSINIT:   EQU     45H
+FUNC_IDLE:      EQU     48H
+FUNC_COMMONBASE: EQU    4BH
+FUNC_SWTUSER:   EQU     4EH
+FUNC_SWTSYS:    EQU     51H
+FUNC_PDISP:     EQU     54H
+FUNC_XDOSENT:   EQU     57H
+FUNC_SYSDAT:    EQU     5AH
+
 ; XDOS function codes
-;
-poll	equ	131		; XDOS poll function
-flagwait equ	132		; XDOS flag wait
-flagset	equ	133		; XDOS flag set
+POLL:           EQU     131     ; XDOS poll function
+FLAGWAIT:       EQU     132     ; XDOS flag wait
+FLAGSET:        EQU     133     ; XDOS flag set
 
-;
-; XIOS jump vector
-;
-;	jmp	coldstart	; not used - commonbase at offset 0
-	jmp	commonbase
-wboot:
-	jmp	warmstart	; warm start
-	jmp	const		; console status
-	jmp	conin		; console character in
-	jmp	conout		; console character out
-	jmp	list		; list character out
-	jmp	rtnempty	; punch not implemented
-	jmp	rtnempty	; reader not implemented
-	jmp	home		; move head to home
-	jmp	seldsk		; select disk
-	jmp	settrk		; set track number
-	jmp	setsec		; set sector number
-	jmp	setdma		; set dma address
-	jmp	read		; read disk
-	jmp	write		; write disk
-	jmp	listst		; list status
-	jmp	sectran		; sector translate
-	jmp	selmemory	; select memory
-	jmp	polldevice	; poll device
-	jmp	startclock	; start clock
-	jmp	stopclock	; stop clock
-	jmp	exitregion	; exit region
-	jmp	maxconsole	; maximum console number
-	jmp	systeminit	; system initialization
-	db	0		; force internal idle dispatch
-;	jmp	idle		; idle procedure
+; Number of consoles
+NMBCNS:         EQU     8       ; 8 consoles for SSH users
 
-;
-; Common base - patched by GENSYS
-;
-commonbase:
-	jmp	coldstart
-swtuser: jmp	$-$		; switch to user bank
-swtsys:  jmp	$-$		; switch to system bank
-pdisp:   jmp	$-$		; MP/M dispatcher
-xdos:	 jmp	$-$		; XDOS entry
-sysdat:  dw	$-$		; system data page address
+; =============================================================================
+; BNKXIOS Jump Vector
+; Entry at offset 0 is COMMONBASE, not COLDBOOT (MP/M II convention)
+; =============================================================================
 
-coldstart:
-warmstart:
-	mvi	c,0
-	jmp	xdos		; system reset, terminate process
+        JP      COMMONBASE      ; 00h - Commonbase (not coldboot)
+WBOOT:  JP      DO_WBOOT        ; 03h - Warm start
+        JP      DO_CONST        ; 06h - Console status
+        JP      DO_CONIN        ; 09h - Console input
+        JP      DO_CONOUT       ; 0Ch - Console output
+        JP      DO_LIST         ; 0Fh - List output
+        JP      DO_PUNCH        ; 12h - Punch output
+        JP      DO_READER       ; 15h - Reader input
+        JP      DO_HOME         ; 18h - Home disk
+        JP      DO_SELDSK       ; 1Bh - Select disk
+        JP      DO_SETTRK       ; 1Eh - Set track
+        JP      DO_SETSEC       ; 21h - Set sector
+        JP      DO_SETDMA       ; 24h - Set DMA address
+        JP      DO_READ         ; 27h - Read sector
+        JP      DO_WRITE        ; 2Ah - Write sector
+        JP      DO_LISTST       ; 2Dh - List status
+        JP      DO_SECTRAN      ; 30h - Sector translate
+        JP      DO_SELMEM       ; 33h - Select memory bank
+        JP      DO_POLLDEV      ; 36h - Poll device
+        JP      DO_STARTCLK     ; 39h - Start clock
+        JP      DO_STOPCLK      ; 3Ch - Stop clock
+        JP      DO_EXITRGN      ; 3Fh - Exit region
+        JP      DO_MAXCON       ; 42h - Max console
+        JP      DO_SYSINIT      ; 45h - System init
+        DB      0               ; 48h - Force internal idle dispatch (0 = use PDISP)
 
-;
-; Console Status - return 0FFH if char ready, 00H if not
-;
-const:
-	; Emulator intercepts at this address
-	; D = console number
-	xra	a		; return 0 (no char ready)
-	ret
+; =============================================================================
+; Commonbase structure - patched by GENSYS, used by XDOS/BNKBDOS
+; These addresses are returned by BOOT and used for bank switching
+; =============================================================================
 
-;
-; Console Input - return character in A
-;
-conin:
-	; Emulator intercepts at this address
-	; D = console number
-	xra	a		; return 0
-	ret
+COMMONBASE:
+        JP      DO_BOOT         ; 4Bh - Cold start (returns HL = commonbase)
+SWTUSER:
+        JP      DO_SWTUSER      ; 4Eh - Switch to user bank
+SWTSYS:
+        JP      DO_SWTSYS       ; 51h - Switch to system bank
+PDISP:
+        JP      DO_PDISP        ; 54h - MP/M dispatcher
+XDOS:
+        JP      DO_XDOSENT      ; 57h - XDOS entry
+SYSDAT:
+        DW      0FF00H          ; 5Ah - System data page address (patched by GENSYS)
 
-;
-; Console Output - C = character to output
-;
-conout:
-	; Emulator intercepts at this address
-	; D = console number, C = character
-	ret
+; =============================================================================
+; Entry point implementations using port dispatch
+; =============================================================================
 
-;
-; List Output - C = character to print
-;
-list:
-	; Emulator intercepts at this address
-	ret
+DO_BOOT:
+        ; Cold boot - returns HL = commonbase address
+        LD      A, FUNC_BOOT
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; List Status - return 0FFH if ready
-;
-listst:
-	mvi	a,0ffh		; always ready
-	ret
+DO_WBOOT:
+        ; Warm boot - terminate process via XDOS
+        LD      C, 0
+        JP      XDOS            ; System reset, terminate process
 
-rtnempty:
-	xra	a
-	ret
+DO_CONST:
+        ; Console status - D = console number
+        ; Returns A = 0FFH if char ready, 00H if not
+        LD      A, FUNC_CONST
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Disk I/O routines - emulator intercepts these
-;
-home:
-	; Move to track 0
-	ret
+DO_CONIN:
+        ; Console input - D = console number
+        ; Returns A = character
+        LD      A, FUNC_CONIN
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-seldsk:
-	; Select disk in C
-	; Return HL = DPH address, or 0 if error
-	mov	a,c
-	cpi	4		; support 4 drives
-	jnc	selerr
-	; Return DPH address (emulator intercepts and returns proper DPH)
-	lxi	h,dph0
-	ret
-selerr:
-	lxi	h,0
-	ret
+DO_CONOUT:
+        ; Console output - D = console number, C = character
+        LD      A, FUNC_CONOUT
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-settrk:
-	; Set track in BC
-	ret
+DO_LIST:
+        ; List output - C = character
+        LD      A, FUNC_LIST
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-setsec:
-	; Set sector in BC
-	ret
+DO_PUNCH:
+        ; Punch output - not implemented
+        XOR     A
+        RET
 
-setdma:
-	; Set DMA address in BC
-	ret
+DO_READER:
+        ; Reader input - not implemented
+        LD      A, 1AH          ; Return EOF (Ctrl-Z)
+        RET
 
-read:
-	; Read sector - return A=0 success, A=1 error
-	; Emulator intercepts at this address
-	xra	a
-	ret
+DO_HOME:
+        ; Home disk - move to track 0
+        LD      A, FUNC_HOME
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-write:
-	; Write sector - C = deblocking code
-	; Return A=0 success, A=1 error
-	; Emulator intercepts at this address
-	xra	a
-	ret
+DO_SELDSK:
+        ; Select disk - C = disk number
+        ; Returns HL = DPH address, or 0 if error
+        LD      A, FUNC_SELDSK
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-sectran:
-	; Translate sector BC using table DE
-	; Return HL = physical sector
-	mov	h,b
-	mov	l,c		; no translation
-	ret
+DO_SETTRK:
+        ; Set track - BC = track number
+        ; Copy to HL for emulator
+        LD      H, B
+        LD      L, C
+        LD      A, FUNC_SETTRK
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Memory bank selection
-;
-selmemory:
-	; BC = address of memory descriptor
-	; Emulator handles bank switching
-	ret
+DO_SETSEC:
+        ; Set sector - BC = sector number
+        ; Copy to HL for emulator
+        LD      H, B
+        LD      L, C
+        LD      A, FUNC_SETSEC
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Device polling
-;
-polldevice:
-	; C = device number
-	; Return 0FFH if ready, 00H if not
-	xra	a
-	ret
+DO_SETDMA:
+        ; Set DMA address - BC = DMA address
+        ; Copy to HL for emulator
+        LD      H, B
+        LD      L, C
+        LD      A, FUNC_SETDMA
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Clock control
-;
-startclock:
-	mvi	a,0ffh
-	sta	tickn
-	ret
+DO_READ:
+        ; Read sector - returns A = 0 success, A = 1 error
+        LD      A, FUNC_READ
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-stopclock:
-	xra	a
-	sta	tickn
-	ret
+DO_WRITE:
+        ; Write sector - C = deblocking code
+        ; Returns A = 0 success, A = 1 error
+        LD      A, FUNC_WRITE
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Exit region - enable interrupts if not preempted
-;
-exitregion:
-	lda	preemp
-	ora	a
-	rnz
-	ei
-	ret
+DO_LISTST:
+        ; List status - returns A = 0FFH if ready
+        LD      A, FUNC_LISTST
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Maximum console number
-;
-maxconsole:
-	mvi	a,nmbcns
-	ret
+DO_SECTRAN:
+        ; Sector translate - BC = logical sector, DE = translation table
+        ; Returns HL = physical sector
+        ; Copy BC to HL for emulator
+        LD      H, B
+        LD      L, C
+        LD      A, FUNC_SECTRAN
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; System initialization
-;
-systeminit:
-	; Set up interrupt handler at RST 7 (0038H)
-	mvi	a,0c3h		; JP opcode
-	sta	0038h
-	lxi	h,inthnd
-	shld	0039h
+DO_SELMEM:
+        ; Select memory bank - C = bank number
+        ; Use dedicated bank select port for efficiency
+        LD      A, C
+        OUT     (BANK_SELECT), A
+        RET
 
-	db	0edh,056h	; IM 1 (Z80 instruction)
-	ei
-	ret
+DO_POLLDEV:
+        ; Poll device - C = device number
+        ; Returns A = 0FFH if ready, 00H if not
+        LD      A, FUNC_POLLDEV
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-;
-; Interrupt handler
-;
-inthnd:
-	push	psw
-	push	b
-	push	d
-	push	h
+DO_STARTCLK:
+        ; Start clock - enable tick interrupts
+        LD      A, 0FFH
+        LD      (TICKN), A
+        LD      A, FUNC_STARTCLK
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-	mvi	a,0ffh
-	sta	preemp		; set preempted flag
+DO_STOPCLK:
+        ; Stop clock - disable tick interrupts
+        XOR     A
+        LD      (TICKN), A
+        LD      A, FUNC_STOPCLK
+        OUT     (XIOS_DISPATCH), A
+        RET
 
-	; Check for clock tick
-	lda	tickn
-	ora	a
-	jz	notick
-	mvi	c,flagset
-	mvi	e,1
-	call	xdos		; set flag #1 (tick)
-notick:
+DO_EXITRGN:
+        ; Exit region - enable interrupts if not preempted
+        LD      A, (PREEMP)
+        OR      A
+        RET     NZ
+        EI
+        RET
 
-	; Update 1-second counter
-	lxi	h,cnt60
-	dcr	m
-	jnz	notsec
-	mvi	m,60
-	mvi	c,flagset
-	mvi	e,2
-	call	xdos		; set flag #2 (1 sec)
-notsec:
+DO_MAXCON:
+        ; Max console - returns A = number of consoles
+        LD      A, NMBCNS
+        RET
 
-	xra	a
-	sta	preemp		; clear preempted flag
+DO_SYSINIT:
+        ; System initialization
+        ; Set up interrupt handler at RST 7 (0038H)
+        LD      A, 0C3H         ; JP opcode
+        LD      (0038H), A
+        LD      HL, INTHND
+        LD      (0039H), HL
 
-	pop	h
-	pop	d
-	pop	b
-	pop	psw
-	ei
-	jmp	pdisp		; dispatch to next process
+        ; Set interrupt mode 1 (Z80)
+        IM      1
+        EI
 
-;
+        ; Notify emulator
+        LD      A, FUNC_SYSINIT
+        OUT     (XIOS_DISPATCH), A
+        RET
+
+DO_SWTUSER:
+        ; Switch to user bank
+        LD      A, FUNC_SWTUSER
+        OUT     (XIOS_DISPATCH), A
+        RET
+
+DO_SWTSYS:
+        ; Switch to system bank
+        LD      A, FUNC_SWTSYS
+        OUT     (XIOS_DISPATCH), A
+        RET
+
+DO_PDISP:
+        ; Process dispatcher - called to switch processes
+        LD      A, FUNC_PDISP
+        OUT     (XIOS_DISPATCH), A
+        RET
+
+DO_XDOSENT:
+        ; XDOS entry point
+        LD      A, FUNC_XDOSENT
+        OUT     (XIOS_DISPATCH), A
+        RET
+
+; =============================================================================
+; Interrupt Handler - 60Hz tick
+; Called via RST 38H (set up by SYSINIT)
+; =============================================================================
+
+INTHND:
+        PUSH    AF
+        PUSH    BC
+        PUSH    DE
+        PUSH    HL
+
+        ; Set preempted flag
+        LD      A, 0FFH
+        LD      (PREEMP), A
+
+        ; Check if clock tick is enabled
+        LD      A, (TICKN)
+        OR      A
+        JR      Z, NOTICK
+
+        ; Set flag #1 (tick flag) via XDOS
+        LD      C, FLAGSET
+        LD      E, 1
+        CALL    XDOS
+
+NOTICK:
+        ; Update 1-second counter (60 ticks = 1 second)
+        LD      HL, CNT60
+        DEC     (HL)
+        JR      NZ, NOTSEC
+
+        ; Reset counter and set flag #2 (1 second flag)
+        LD      (HL), 60
+        LD      C, FLAGSET
+        LD      E, 2
+        CALL    XDOS
+
+NOTSEC:
+        ; Clear preempted flag
+        XOR     A
+        LD      (PREEMP), A
+
+        POP     HL
+        POP     DE
+        POP     BC
+        POP     AF
+        EI
+        JP      PDISP           ; Dispatch to next ready process
+
+; =============================================================================
 ; Data area
-;
-tickn:	db	0		; tick enable flag
-cnt60:	db	60		; 60 Hz counter
-preemp:	db	0		; preempted flag
+; =============================================================================
 
-;
-; Disk Parameter Headers (minimal - emulator provides real data)
-;
-dph0:	dw	0		; XLT
-	dw	0,0,0		; scratch
-	dw	dirbuf		; DIRBUF
-	dw	dpb		; DPB
-	dw	0		; CSV
-	dw	alv0		; ALV
+TICKN:  DB      0               ; Tick enable flag
+CNT60:  DB      60              ; 60 Hz counter (for 1-second flag)
+PREEMP: DB      0               ; Preempted flag
 
-dpb:	dw	64		; SPT
-	db	5		; BSH
-	db	31		; BLM
-	db	1		; EXM
-	dw	2039		; DSM
-	dw	1023		; DRM
-	db	0ffh		; AL0
-	db	0ffh		; AL1
-	dw	0		; CKS
-	dw	2		; OFF
+; =============================================================================
+; Disk Parameter Headers and DPBs
+; The emulator provides actual disk parameters via SELDSK return value
+; These are minimal placeholders for compatibility
+; =============================================================================
 
-dirbuf:	ds	128
-alv0:	ds	256
+DPH_TABLE:
+; DPH for drive A
+DPH0:
+        DW      0               ; XLT (no translation)
+        DW      0, 0, 0         ; Scratch area
+        DW      DIRBUF          ; DIRBUF address
+        DW      DPB_8MB         ; DPB address
+        DW      0               ; CSV (not used)
+        DW      ALV0            ; ALV address
 
-	db	0		; force last byte
+; DPH for drive B
+DPH1:
+        DW      0
+        DW      0, 0, 0
+        DW      DIRBUF
+        DW      DPB_8MB
+        DW      0
+        DW      ALV1
 
-	end
+; DPH for drive C
+DPH2:
+        DW      0
+        DW      0, 0, 0
+        DW      DIRBUF
+        DW      DPB_8MB
+        DW      0
+        DW      ALV2
+
+; DPH for drive D
+DPH3:
+        DW      0
+        DW      0, 0, 0
+        DW      DIRBUF
+        DW      DPB_8MB
+        DW      0
+        DW      ALV3
+
+; Disk Parameter Block for hd1k format (8MB)
+DPB_8MB:
+        DW      64              ; SPT - 64 sectors per track (128-byte logical)
+        DB      5               ; BSH - block shift (4K blocks)
+        DB      31              ; BLM - block mask
+        DB      1               ; EXM - extent mask
+        DW      2039            ; DSM - disk size - 1 in blocks
+        DW      1023            ; DRM - directory max - 1
+        DB      0FFH            ; AL0 - 8 directory blocks allocated
+        DB      0FFH            ; AL1
+        DW      0               ; CKS - checksum vector size (0 = fixed disk)
+        DW      2               ; OFF - reserved tracks
+
+; DPB for 8" SSSD (IBM 3740 standard - 77 tracks, 26 sectors, 128 bytes)
+DPB_SSSD:
+        DW      26              ; SPT - sectors per track
+        DB      3               ; BSH - block shift (1K blocks)
+        DB      7               ; BLM - block mask
+        DB      0               ; EXM - extent mask
+        DW      242             ; DSM - disk size - 1 in blocks
+        DW      63              ; DRM - directory max - 1
+        DB      0C0H            ; AL0 - 2 directory blocks
+        DB      000H            ; AL1
+        DW      16              ; CKS - checksum vector size
+        DW      2               ; OFF - reserved tracks
+
+; Buffers
+DIRBUF: DS      128             ; Directory buffer (shared)
+ALV0:   DS      256             ; Allocation vector drive A
+ALV1:   DS      256             ; Allocation vector drive B
+ALV2:   DS      256             ; Allocation vector drive C
+ALV3:   DS      256             ; Allocation vector drive D
+
+        END
